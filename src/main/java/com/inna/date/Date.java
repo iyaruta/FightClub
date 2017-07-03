73b44b493bf0a0f5cc06c9d3916af15ba58a2a60
Inna <i.yaruta@hotmail.com>
2017-07-03 19:15:31 +0300

    Initial commit

---
+++ src/main/java/com/inna/date/Date.java
@@ -0,0 +1,77 @@
+package com.inna.date;
+
+public class Date {
+
+    private String name;
+    private int force;
+    private int skill;
+    private int instinct;
+    private int stamina;
+    private int experience;
+    private int level;
+    private int health;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
+
+    public int getSkill() {
+        return skill;
+    }
+
+    public void setSkill(int skill) {
+        this.skill = skill;
+    }
+
+    public int getInstinct() {
+        return instinct;
+    }
+
+    public void setInstinct(int instinct) {
+        this.instinct = instinct;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
