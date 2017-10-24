3249ae4ad804d9d5b231430c5b23919f8a973e9a
Inna <i.yaruta@hotmail.com>
2017-10-24 16:31:40 +0300

    Refactoring of spring data and configurations

--- src/main/java/home/inna/fc/auth/HeroAuth.java
+++ src/main/java/home/inna/fc/auth/HeroAuth.java
@@ -1,32 +1,11 @@
 package home.inna.fc.auth;
 
+import lombok.Value;
+
+@Value
 public class HeroAuth {
 
     private Long id;
     private String name;
-
-    public HeroAuth() {
-    }
-
-    public HeroAuth(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
--- src/main/java/home/inna/fc/controller/SignUpController.java
+++ src/main/java/home/inna/fc/controller/SignUpController.java
@@ -3,6 +3,7 @@ package home.inna.fc.controller;
 import home.inna.fc.data.Account;
 import home.inna.fc.repository.AccountRepository;
 import home.inna.fc.service.HeroService;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,34 +37,11 @@ public class SignUpController {
         heroService.save(account.getId(), signUp.getName());
     }
 
+    @Data
     public static class SignUp {
         private String email;
         private String password;
         private String name;
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
     }
 
 
